ee02dc72bf5bbbfebc6dc07f1ec71993dc43733a
gggsiw <168559646+gggsiw@users.noreply.github.com>
2026-04-21 13:50:26 +0530

    Change Uvicorn app reference and port number

--- app.py
+++ app.py
@@ -183,8 +183,7 @@ def play():
 
 
 def main():
-    uvicorn.run("server.app:app", host="0.0.0.0", port=7880, reload=False)
-
+    uvicorn.run("app:app", host="0.0.0.0", port=7860, reload=False)
 
 if __name__ == "__main__":
     main()
